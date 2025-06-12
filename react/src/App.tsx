1dba8ce4fd5369c26f7dab8bf241c56dd0d54815
Fatai Jabar <aabdfatahi@gmail.com>
2025-06-12 22:11:37 +0100

    initial commit

---
+++ react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center text-lg m-4 font-bold text-pink-400">
+      <h1>Hello Habike Success is Yours 💖😘</h1>
+    </div>
+  );
+}
+
+export default App;
